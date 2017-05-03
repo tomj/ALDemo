d2223f6ea33d76e369e463a9df454ac06e46f45d
Tom Jowett <tom@tomjowett.com>
2017-05-03 14:09:16 +0800

    initial commit

--- ALDemo/ViewController.swift
+++ ALDemo/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Do any additional setup after loading the view, typically from a nib. Do any additional setup after loading the view, typically from a nib. Do any additional setup after loading the view, typically from a nib. Do any additional setup after loading the view, typically from a nib."
     }
 
     override func didReceiveMemoryWarning() {
